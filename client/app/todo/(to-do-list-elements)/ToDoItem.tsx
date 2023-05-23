800155ed87b1ab948a0206030ae7fda1865564a3
Max Xue <maxxue1512@gmail.com>
2023-05-23 14:23:23 +1000

    Updating styling for to do item

--- client/app/todo/(to-do-list-elements)/ToDoItem.tsx
+++ client/app/todo/(to-do-list-elements)/ToDoItem.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function ToDoItem() {
 	return (
 		<div className='p-2 mb-2 border-box-shadow'>
-			{/* use hover:shadow-lg when dragging */}
-			ToDoItem
+			{/* use shadow-raised utility class when dragging */}
+			<Checkbox />
+			<span>To Do Item Title</span>
 		</div>
 	);
 }
